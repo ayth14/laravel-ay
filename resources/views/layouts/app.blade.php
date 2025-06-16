909aa63dbc0c966ccaaa4c8eededec2fd8f64701
aprashant14-metadrob <aprashant@metadrob.com>
2025-06-16 14:12:28 +0530

    Related task(s): cancel button added and alpine js interaction added with falsh message styling.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 12 Task List | @yield('page-title')</title>
-
+    <script src="https://kit.fontawesome.com/e04637118f.js" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     {{-- Tailwind-Cdn ONLY FOR DEVELOPMENT --}}
     {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> --}}
 
@@ -16,16 +17,27 @@
 </head>
 
 <body class="container mx-auto mt-7 px-5">
-    @if (session()->has('success'))
-        <div>
-            {{ session('success') }}
-        </div>
-    @endif
     <h1 class="text-3xl font-bold">@yield('title')</h1>
+
+    @if (session()->has('success'))
+        {{-- alpine js container for enabling the  interaction for the content --}}
+        <div x-data="{ flash: true }">
+            <div x-show="flash"
+                class="relative rounded border border-green-400 bg-green-200 px-4 py-3 text-lg text-green-700 max-w-lg my-5"
+                role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div class="">{{ session('success') }}</div>
+                <button class="absolute right-4 top-2 cursor-pointer" @click="flash=false">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+    @endif
+
 
     <main>
         @yield('main-content')
     </main>
+    </div>
 </body>
 
 </html>
--- resources/views/tasks/form.blade.php
+++ resources/views/tasks/form.blade.php
@@ -12,7 +12,10 @@
         @endisset
         <div class="mb-2">
             <label for="title">Title</label>
-            <input class="input-form" type="text" id="title" name="title" placeholder="Enter the task title"
+            <input class="input-form @error('title')
+              border-red-500
+            @enderror " type="text"
+                id="title" {{-- @class(['border-red-500' => $errors->has('title')]) --}} name="title" placeholder="Enter the task title"
                 value="{{ $task->title ?? old('title') }}" />
             @error('title')
                 <span class="error-msg">{{ $message }}</span>
@@ -20,25 +23,32 @@
         </div>
         <div class="mb-2">
             <label for="description">Description</label>
-            <Textarea id="description" name="description" rows="5" placeholder="Enter the task description">{{ $task->description ?? old('description') }}</Textarea>
+            <Textarea id="description" name="description" rows="5" @class(['border-red-500' => $errors->has('description')])
+                placeholder="Enter the task description">{{ $task->description ?? old('description') }}</Textarea>
             @error('description')
                 <span class="error-msg">{{ $message }}</span>
             @enderror
         </div>
         <div class="mb-2">
             <label for="long_description">Long Description</label>
-            <Textarea id="long_description" name="long_description" rows="8" placeholder="Enter Long Description">{{ $task->long_description ?? old('long_description') }}</Textarea>
+            <Textarea id="long_description" name="long_description" rows="8" @class(['border-red-500' => $errors->has('long_description')])
+                placeholder="Enter Long Description">{{ $task->long_description ?? old('long_description') }}</Textarea>
             @error('long_description')
                 <span class="error-msg">{{ $message }}</span>
             @enderror
         </div>
 
-        <button type="submit" class="btn-save">
-            @isset($task)
-                Update Task
-            @else
-                Add Task
-            @endisset
-        </button>
+        <div class="flex items-center gap-3">
+
+            <button type="submit" class="btn-save">
+                @isset($task)
+                    Update Task
+                @else
+                    Add Task
+                @endisset
+            </button>
+
+            <a href="{{ route('tasks.index') }}" class="link mt-5">Cancel</a>
+        </div>
     </form>
 @endsection
